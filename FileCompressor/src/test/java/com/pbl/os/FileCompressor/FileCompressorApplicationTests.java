d2190c8d6f9f8ad673e0737eba774b9456121f22
Rahul Kumar Singh <87928472+rahulsingh7111711@users.noreply.github.com>
2025-06-02 15:05:39 +0530

    Add files via upload

---
+++ FileCompressor/src/test/java/com/pbl/os/FileCompressor/FileCompressorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbl.os.FileCompressor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileCompressorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
